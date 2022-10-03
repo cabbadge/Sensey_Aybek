203e7b9c9989e24598faa307c67e4f39097cd79f
sher <sher.kg15@gmail.com>
2022-10-03 11:58:13 +0600

    Проверка

---
+++ js/script.js
@@ -0,0 +1,64 @@
+// (function (){
+//     'use strict';
+// })();
+
+
+
+// const box = document.getElementById('box');
+// console.log(box);
+
+// const button= document.getElementsByTagName('button');
+// console.log(button[1]);
+
+// const circle = document.getElementsByClassName('circle');
+// console.log(circle[2]);
+
+// const heart= document.querySelectorAll('.heart');
+// const arr = ['sher', 'Aybek'];
+
+// // const res = arr.forEach(item => {
+// //     console.log(item);
+// //     return item;
+// // });
+
+// const res = arr.map(item => {
+//     console.log(item);
+//     return "I'm " + item;
+// });
+
+// const obj = {
+//     a: 1,
+//     b: 2,
+// };
+
+// const usernames = ['aybek', 'sher'];
+
+// const users ={
+//     aybek: 1,
+//     sher: 2,
+// };
+
+// usernames.forEach(item => {
+//     // let key = prompt('Кто вам нужен?', 'name');
+//    console.log(users[item]);
+//    return item;
+// });
+
+// const map =
+
+
+
+
+
+// console.log({res});
+// // forEach => undfnd
+// // map => []
+
+
+// const heart1=document.querySelector('.heart');
+// console.log(heart1);
+
+
+
+const sher = document.getElementById('box');
+
